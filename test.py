3ebbd5e6ebb454acbcd9170216dbe957a541bb82
vikrantballal <vikrantballal647@gmail.com>
2025-09-25 21:47:50 +0530

    test.py

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name="Tannu"
         upper_name=to_upper(name)
-        self.assertEqual(upper_name,"Tannu")
+        self.assertEqual(upper_name,"Vikrant")
 
 if __name__=='__main__':
+
     unittest.main()
